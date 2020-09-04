ffa23e8c3007125432bedc9c8bc251f854a1d459
NaviHX <932942142@qq.com>
2020-09-04 17:02:59 +0800

    add 1000

---
+++ hard/1000/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mergeStones(vector<int>& stones, int K) {
+        int n=stones.size();
+        if(n<=1) return 0;
+        if((n-1)%(K-1)!=0)
+            return -1;
+        int pre[31]={0};
+        for(int i=1;i<=n;i++)
+            pre[i]=pre[i-1]+stones[i-1];
+        int dp[n][n];
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                dp[i][j]=0;
+        for(int r=0;r<n;r++)
+            for(int l=r-K+1;l>=0;l--)
+            {
+                int m=0x7fffffff;
+                for(int p=r;p>l;p-=(K-1))
+                    m=min(m,dp[l][p-1]+dp[p][r]);
+                dp[l][r]=m;
+                if((r-l)%(K-1)==0)
+                    dp[l][r]+=pre[r+1]-pre[l];
+            }
+        return dp[0][n-1];
+    }
+};
