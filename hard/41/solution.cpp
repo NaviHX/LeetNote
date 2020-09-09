391572472c448f7ea3badc3a35f05c38e0f445d6
NaviHX <932942142@qq.com>
2020-09-09 16:01:47 +0800

    add 41

---
+++ hard/41/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        if(nums.empty())
+            return 1;
+        int n = nums.size();
+        for (int i = 0; i < n;i++)
+            if(nums[i]<=0)
+                nums[i] = n + 1;
+        for (int i = 0; i < n;i++)
+        {
+            int temp = abs(nums[i]);
+            if(temp<=n)
+                nums[temp - 1] = -abs(nums[temp - 1]);
+        }
+        for (int i = 0; i < n;i++)
+            if(nums[i]>0)
+                return i + 1;
+        return n+1;
+    }
+};
